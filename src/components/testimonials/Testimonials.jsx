3fe0f523184c971691ab33d37afdeeb8a10a7a7b
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-20 23:11:43 +0300

    finish designing right side

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,11 @@
 import "./testimonial.css";
-import React,{useState} from "react";
-import { testimonialsData } from './../../data/testimonialsData';
-
+import React, { useState } from "react";
+import { testimonialsData } from "./../../data/testimonialsData";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
 const Testimonials = () => {
-    const [selected,setSelected] = useState(0);
-    const tlength = testimonialsData.length;
+  const [selected, setSelected] = useState(0);
+  const tlength = testimonialsData.length;
 
   return (
     <div className="testimonials">
@@ -12,19 +13,21 @@ const Testimonials = () => {
         <span>Testimonials</span>
         <span className="stroke-text">What they</span>
         <span>say about us</span>
+        <span>{testimonialsData[selected].review}</span>
         <span>
-            {testimonialsData[selected].review}
-        </span>
-        <span>
-            <span style={{color:'var(--orange)'}}> 
-                { testimonialsData[selected].name}
-            </span>{" "}
-           
-            {testimonialsData[selected].status}
-            
+          <span style={{ color: "var(--orange)" }}>
+            {testimonialsData[selected].name}
+          </span>{" "}
+          {testimonialsData[selected].status}
         </span>
       </div>
-      <div className="right-t"></div>
+      <div className="right-t">
+        <img src={testimonialsData[selected].image} alt="" />
+        <div className="arrows">
+          <img src={leftArrow} alt="" />
+          <img src={rightArrow} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
