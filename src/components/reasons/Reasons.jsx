269dd0e5556fdce5fa0300a755aa23b411123fe5
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-20 17:46:49 +0300

    finish the reasons section

--- src/components/reasons/Reasons.jsx
+++ src/components/reasons/Reasons.jsx
@@ -18,9 +18,39 @@ const Reasons = () => {
             <img src={image4} alt="" />
          </div>
          <div className="right-r">
-            Right Side
+            <span>some reasons</span>
+            <div>
+            <span className="stroke-text">Why</span>
+            <span> choose us?</span>
+            </div>
+            {/* details */}
+            <div className="details-r">
+            <div>
+            <img src={tick} alt=""></img>
+            <span>OVER 140+ EXPERT COACHES</span>
+            </div>
+            <div>
+            <img src={tick} alt="" />
+            <span>TRAIN FASTER AND SMARTER THAN BEFORE</span>
+            </div>
+            <div>
+            <img src={tick} alt="" />
+            <span>FREE PROGRAM FOR NEW MEMBER</span></div>
+            <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span></div>
+         </div>
+          <span style={
+            {color: "var(--gray)",
+            fontWeight : "normal"
+          }}>OUR PARTNERS</span>
+          <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" /></div>
          </div>
     </div>
+
   )
 }
 
